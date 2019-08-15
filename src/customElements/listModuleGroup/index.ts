bea9b2e5201552fee924a2f0817e23623e37290b
Erwin Goossen <erwin@navelpluisje.nl>
2019-08-15 21:00:21 +0200

    made groups collapsable

--- src/customElements/listModuleGroup/index.ts
+++ src/customElements/listModuleGroup/index.ts
@@ -4,6 +4,11 @@ import style from './style.css'
 class ListModuleGroup extends HTMLElement {
   title: string
   name: string
+  height: string
+  titleDOM: HTMLHeadingElement
+  listDOM: HTMLUListElement
+  listContainerDOM: HTMLElement
+  open: boolean = false
 
   constructor() {
     super()
@@ -28,15 +33,30 @@ class ListModuleGroup extends HTMLElement {
     this.title = this.getAttribute('group') || ''
     this.name = this.getAttribute('name') || ''
 
-    this.shadowRoot.querySelector('h4').textContent = this.title
-    this.shadowRoot.querySelector('dd').className = this.name
-    this.shadowRoot.querySelector('ul').className = this.name
+    this.titleDOM = this.shadowRoot.querySelector('h4')
+    this.titleDOM.textContent = this.title
 
-    // this.setEventBindings()
+    this.listContainerDOM = this.shadowRoot.querySelector('#list')
+    this.listDOM = this.shadowRoot.querySelector('ul')
+    this.listDOM.className = this.name
+
+    this.shadowRoot.querySelector('dd').className = this.name
+
+    this.setEventBindings()
+  }
+
+  toggleList = () => {
+    if (this.open) {
+      this.listContainerDOM.style.height = '0px'
+    } else {
+      const height = this.listDOM.getBoundingClientRect().height
+      this.listContainerDOM.style.height = `${height}px`
+    }
+    this.open = !this.open
   }
 
   setEventBindings() {
-    // this.button.addEventListener('click', () => this.dispatchEvent(this.clickEvent))
+    this.titleDOM.addEventListener('click', this.toggleList)
   }
 }
 
