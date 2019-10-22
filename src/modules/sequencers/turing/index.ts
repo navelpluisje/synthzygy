db09fe867220ca1338f34fee752775107af1a466
Erwin Goossen <erwin@navelpluisje.nl>
2019-10-22 20:23:58 +0200

    Refactor knob initialisation. Turing fixed

--- src/modules/sequencers/turing/index.ts
+++ src/modules/sequencers/turing/index.ts
@@ -1,8 +1,8 @@
-import { Knob, SynthModule } from '@components/index';
+import { SynthModule } from '@components/index';
 import { ParentModule } from '@interfaces/index';
 import { GateNode } from '@nodes/gateNode';
 import { Colors } from 'src/constants/enums';
-import { GateTrigger, PositionType } from 'src/types';
+import { GateTrigger, ModuleDefaultValues, PositionType } from 'src/types';
 import { ModuleBase } from '../../moduleBase';
 import { controlTypes } from './controls';
 import { inputTypes } from './inputs';
@@ -14,32 +14,31 @@ export class TuringMachine extends ModuleBase implements ParentModule {
     height: 230,
     width: 190,
   };
-  public static pulsDimensions = {
-    height: 200,
-    width: 90,
-  };
+  // tslint:disable-next-line
+  public probKey = "prob'ty";
 
   public type =  'turinger';
   public title =  'Turing Machine';
   public active: boolean = false;
-  public node: TuringMachineNode;
+  protected defaults: ModuleDefaultValues = {
+    [this.probKey]: 6,
+    length: 6,
+  };
+  private node: TuringMachineNode;
 
-  constructor(canvas: CanvasRenderingContext2D, context: AudioContext, position: PositionType) {
+  constructor(
+    canvas: CanvasRenderingContext2D,
+    context: AudioContext,
+    position: PositionType,
+    defaults: ModuleDefaultValues,
+  ) {
     super(canvas, position);
+    this.accentColor = Colors.AccentUtility;
     this.node = new TuringMachineNode(context);
     this.container = new SynthModule(canvas, TuringMachine.dimensions, position, this.color);
     this.addInputs(inputTypes, this.getInputConnection);
     this.addOutputs(outputTypes, this.getOutputConnection);
-    this.addControls();
-  }
-
-  public getNode() {
-    return this.node;
-  }
-
-  private addControls() {
-    this.controls.push(new Knob(this.canvas, this, controlTypes[0], this.node.setProbability, Colors.AccentUtility));
-    this.controls.push(new Knob(this.canvas, this, controlTypes[1], this.node.setLength, Colors.AccentUtility));
+    this.addKnobs(controlTypes, this.getKnobCallbackAndDefault);
   }
 
   private getOutputConnection = (type: string): ConstantSourceNode | GateNode => {
@@ -62,6 +61,22 @@ export class TuringMachine extends ModuleBase implements ParentModule {
     switch (type) {
       case 'Clock':
         return this.node.inputClock();
+    }
+  }
+
+  private getKnobCallbackAndDefault = (label: string): any => {
+    const key = label.toLowerCase();
+    switch (key) {
+      case this.probKey:
+        return {
+          callback: this.node.setProbability,
+          default: this.defaults[key],
+        };
+      case 'length':
+        return {
+          callback: this.node.setLength,
+          default: this.defaults[key],
+        };
     }
   }
 }
